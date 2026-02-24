e8055bc7fdb8c5f2c2aca661f5c45e98d38d0d3a
antonina p <zelenyzar@gmail.com>
2026-02-24 21:01:39 +0300

    homework 9.2 done

--- src/widget.py
+++ src/widget.py
@@ -1,15 +1,17 @@
 from src.masks import get_mask_account, get_mask_card_number
 
+
 def mask_account_card(number_card: str) -> str:
-    """ Функция, которая принимает либо номер счета, либо номер карты """
+    """Функция, которая принимает либо номер счета, либо номер карты"""
     if "счет" in number_card.lower():
         return f"Счет {get_mask_account(number_card[-20:])}"
     else:
         return f"{number_card[:-17]} {get_mask_card_number(number_card[-16:])}"
 
+
 def get_date(some_date: str) -> str:
-    """ Функция, которая преобразовывает в формат ДД.ММ.ГГГГ """
+    """Функция, которая преобразовывает в формат ДД.ММ.ГГГГ"""
     # 2024-03-11T02:26:18.671407
-    date_a = some_date.split("T")
-    date_b = date_a[0].split("-")
-    return f"{date_b[2]}.{date_b[1]}.{date_b[0]}"
+    date_original = some_date.split("T")
+    date_fixed = date_original[0].split("-")
+    return f"{date_fixed[2]}.{date_fixed[1]}.{date_fixed[0]}"
