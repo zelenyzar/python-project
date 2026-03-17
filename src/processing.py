bbf21216d8eafbcc7d9a8d1687b51c14ead998d3
antonina p <zelenyzar@gmail.com>
2026-03-17 22:34:34 +0300

    add function sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -10,7 +10,10 @@ def filter_by_state(list_dict: list[dict[str: Any]], state: str = "EXECUTED") ->
     return list_dict_new
 
 
-
+def sort_by_date(list_dict: list[dict[str: Any]], sort_option: bool = True) -> list[dict[str: Any]]:
+    """Функция, которая сортирует список по дате в порядке убывания"""
+    list_dict_new = sorted(list_dict, key=lambda k: k["date"], reverse=sort_option)
+    return list_dict_new
 
 # if __name__ == '__main__':
 #     result_dict = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
