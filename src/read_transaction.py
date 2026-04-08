4caaeece726c7431f6c01c161678b17739c055c8
antonina p <zelenyzar@gmail.com>
2026-04-08 23:03:57 +0300

    add read_csv and read_xlsx

---
+++ src/read_transaction.py
@@ -0,0 +1,27 @@
+import csv
+from typing import Any
+
+import pandas as pd
+
+
+def read_csv(filename: str) -> list[dict[str:Any]]:
+    """Функция чтения csv-файла"""
+    list_transactions = []
+    try:
+        with open(filename, "r", encoding="utf-8") as f:
+            reader = csv.DictReader(f, delimiter=";")
+            for row in reader:
+                list_transactions.append(row)
+    except FileNotFoundError as e:
+        print(e)
+    return list_transactions
+
+
+def read_xlsx(filename: str) -> list[dict[str:Any]]:
+    """Функция чтения exсel-файла"""
+    try:
+        df_transactions = pd.read_excel(filename)
+        dict_transactions = df_transactions.to_dict(orient="records")
+        return dict_transactions
+    except FileNotFoundError:
+        return []
